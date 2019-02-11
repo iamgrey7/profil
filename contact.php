0a52da7efcdfd826994257cfc256b5a2b8bef5f5
Egry Yudanegara <whitesideoflove.grey@gmail.com>
2019-02-11 13:23:36 +0700

    lat javascript form kontak

--- contact.php
+++ contact.php
@@ -32,19 +32,86 @@
             </ul>
         </div>
         <!-- Content -->
-        <div class="konten">            
-            <h1 style="text-align:center">Kontak Kami</h1>
-            <h2 style="text-align:center">Di sini 089996212314</h2>
+        <div class="konten">
+            <div class="">
+                <h1 class="judulkiri">Contact Us</h1>
+            </div>
+            <div>
+                <p>Silahkan isi form dibawah ini untuk menghubungi administrator. </p>
+                <p>Kami akan langsung merespon pesan yang anda kirimkan.</p>                 
+            </div>
             
+            <div class="container">
+                <form id="form_kontak">
+                    <label for="nama">Nama Lengkap</label>
+                    <input type="text" id="nama" name="nama" placeholder="Nama Lengkap Anda ..">
+                    <label for="nama">E-Mail</label>
+
+                    <!-- <input type="email" id="email" name="email" placeholder="Alamat E-Mail .."> -->
+                    <input type="text" id="email" name="email" onblur="validasiEmail(this)"
+                    placeholder="Alamat E-Mail ..">                         
+
+                    <label for="pesan">Pesan</label>
+                    <textarea id="pesan" name="pesan" placeholder="Silahkan tulis pesan anda .." 
+                    style="height:200px"></textarea>
+
+                    <!-- <input type="submit" value="Kirim Pesan"> -->
+                    <button class="kirim" id="btn_kirim" name="btn_kirim" onClick="validasi()"> Kirim Pesan</button>
+                </form>
+            </div>         
                        
         </div>
 
     </div>
+
     <!-- Footer -->
     <div class="footer">
         &copy Copyright Egry Yudanegara 2018
     </div>
 
+<script type="text/javascript">
+
+    function validasiEmail(email){
+            var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+            if (reg.test(email.value) == false) 
+            {
+                alert('Email yang anda input salah');
+                email.value = "";
+                // email.focus();
+                return false;
+            } else {
+                return true;
+            }
+            
+    }
+
+   
+    function validasi(){
+        var name = document.getElementById("nama").value;
+        var email = document.getElementById("email").value;
+        var pesan = document.getElementById("pesan").value;
+        
+        if (name == ""){
+            alert ("Cantumkan nama anda");
+            // nama.focus();
+        } else if (email ==""){
+            alert ("Silakan isi email anda");           
+            // email.focus();
+        } else if (pesan == ""){
+            alert("Tuliskan pesan anda kepada kami");
+            // pesan.focus();
+        } else {          
+            alert(
+                "Pesan anda telah dikirim \n"+
+                "Nama : "+name+"\n"+              
+                "E-Mail : "+email+"\n"+
+                "Pesan : "+pesan
+                );     
+        }  
+    };
+
+</script>
 
 
 </body>
---
+++ js/script.js
@@ -0,0 +1,8 @@
+  
+  function testString(){
+    var test = "some string";
+    alert(test[7]); // shows letter 'r'
+    alert(test.charAt(5)); // shows letter 's'
+    alert("test".charAt(1)); //shows letter 'e'
+    alert("test".substring(1,3)); //shows 'es'
+  }
